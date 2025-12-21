6ef16a607cd488fd7f7cc1b6c605c8a4d8eff3e4
bannna2278 <lpbogart2011@gmail.com>
2025-12-21 14:48:59 -0500

    Add files via upload
    V1.1.1B

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-document.getElementById("day1").src="https://www.spc.noaa.gov/products/outlook/day1otlk.html";
+const img = document.getElementById("day1");
+function loadDay(day) {
+    img.src = `https://www.spc.noaa.gov/products/outlook/day${day}otlk.html`;
+}
